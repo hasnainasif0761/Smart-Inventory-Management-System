b331485f006033dd33c8f823cabc93ced27041a0
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-12 13:57:47 +0500

    7-Updation

---
+++ login.php
@@ -0,0 +1,27 @@
+            <!-- Login Side  -->
+             <div class="w-1/2 flex">
+                <!-- Info Panel -->
+                 <div class="w-1/2 bg-blue-500 text-white flex flex-col justify-center items-center text-center p-10">
+                    <h2 class="text-3xl font-bold mb-4">Hello Friends!</h2>
+                    <p class="mb-6">Don't have an account</p>
+                    <button onclick="toggle()"
+                    class="border border-white px-6 py-2 rounded-lg hover:bg-white hover:text-blue-500 transition"
+                    >
+                        Go Regiter Form
+                    </button>
+                 </div>
+
+                 <!-- Login Form -->
+                  <div class="w-1/2 flex items-center justify-center bg-white">
+                    <div class="w-3/4">
+                        <h2 class="font-bold text-3xl mb-6 text-center">Login Form</h2>
+                        <form>
+                            <label class="ml-1">Email :</label>
+                            <input type="email" placeholder="Enter your email" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <label class="ml-1">Password :</label>
+                            <input type="password" placeholder="Enter your password" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <button class="w-full mt-1 mb-4 p-3 bg-blue-400 rounded-lg text-white">Login Now</button>
+                        </form>
+                    </div>
+                  </div>
+             </div>
--- register.php
+++ register.php
@@ -1,3 +1,31 @@
+<?php
+include('./connection.php');
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $secure_pass = md5($password);
+
+    $query = "SELECT * FROM users where userEmail = '$email'";
+    $result = mysqli_query($conn,$query);
+
+    if(mysqli_num_rows($result) > 0){
+        echo "<script>alert('User Already Register ⚠️')</script>";
+    }else{
+        $insert_data = "INSERT into users (userName,userEmail,user_password)VALUES('$name','$email','$secure_pass')";
+
+         if (mysqli_query($conn, $insert_data)) {
+            echo "<script>alert('Registeration Successfully 🤩😍')</script>";
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,14 +45,14 @@
                 <div class="w-1/2 flex items-center justify-center bg-red-100">
                     <div class="w-3/4">
                         <h2 class="text-3xl font-bold mb-4 text-center">Register Form</h2>
-                        <form>
+                        <form method="post">
                             <label class="ml-1">Name :</label>
-                            <input type="text" placeholder="Enter your name" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <input type="text" placeholder="Enter your name" name="name" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
                             <label class="ml-1">Email :</label>
-                            <input type="email" placeholder="Enter your email" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <input type="email" placeholder="Enter your email" name="email" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
                             <label class="ml-1">Password :</label>
-                            <input type="password" placeholder="Enter your password" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-                            <button class="w-full mt-1 mb-4 p-3 bg-blue-400 rounded-lg text-white">Register Now</button>
+                            <input type="password" placeholder="Enter your password" name="password" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <button type="submit" name="submit" class="w-full mt-1 mb-4 p-3 bg-blue-400 rounded-lg text-white">Register Now</button>
                         </form>
                     </div>
                 </div>
@@ -39,34 +67,9 @@
                     </button>
                 </div>
         </div>
-
-            <!-- Login Side  -->
-             <div class="w-1/2 flex">
-                <!-- Info Panel -->
-                 <div class="w-1/2 bg-blue-500 text-white flex flex-col justify-center items-center text-center p-10">
-                    <h2 class="text-3xl font-bold mb-4">Hello Friends!</h2>
-                    <p class="mb-6">Don't have an account</p>
-                    <button onclick="toggle()"
-                    class="border border-white px-6 py-2 rounded-lg hover:bg-white hover:text-blue-500 transition"
-                    >
-                        Go Regiter Form
-                    </button>
-                 </div>
-
-                 <!-- Login Form -->
-                  <div class="w-1/2 flex items-center justify-center bg-white">
-                    <div class="w-3/4">
-                        <h2 class="font-bold text-3xl mb-6 text-center">Login Form</h2>
-                        <form>
-                            <label class="ml-1">Email :</label>
-                            <input type="email" placeholder="Enter your email" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-                            <label class="ml-1">Password :</label>
-                            <input type="password" placeholder="Enter your password" class="w-full mt-1 mb-2 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-                            <button class="w-full mt-1 mb-4 p-3 bg-blue-400 rounded-lg text-white">Login Now</button>
-                        </form>
-                    </div>
-                  </div>
-             </div>
+        <?php
+            include('./login.php')
+        ?>
         </div>
     </div>
     <script>
